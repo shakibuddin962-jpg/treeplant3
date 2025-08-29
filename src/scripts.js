bdd3be6c3b0203a39681bb65367c66c989019e02
shakibuddin962-jpg <shakibuddin962@gmail.com>
2025-08-29 20:11:34 +0600

    Add files via upload

---
+++ src/scripts.js
@@ -0,0 +1,8 @@
+let mobail_icon = document.getElementById("mobail_icon");
+let main_menu = document.getElementById("main_menu");
+
+
+mobail_icon.addEventListener("click", function(){
+      main_menu.classList.toggle("hidden")
+
+})
